7c93798a84d22f36c1d94b3d7fbfb04d02cf599d
harshkakaria <harshkakaria1@gmail.com>
2021-01-09 21:39:49 +0530

    first commit+README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+Guys Check out my YT this is just one of my school projects.
---
+++ app.js
@@ -0,0 +1,33 @@
+let container = document.querySelector('.container');
+let canvas = document.querySelector('.canvas')
+let columnOne = document.querySelector('.one')
+let columnTwo = document.querySelector('.two')
+let columnThree = document.querySelector('.three')
+let columnFour = document.querySelector('.four')
+
+
+for(let i = 1; i <= 9; i++){
+    let imgDiv = document.createElement('div');
+    let divImage = document.createElement('img');
+    divImage.setAttribute('src', `./images/${i}.jpg`)
+    imgDiv.appendChild(divImage);
+
+    if (i < 5){
+        columnOne.appendChild(imgDiv);
+    }else if (i < 9){
+        columnTwo.appendChild(imgDiv);
+    }else if (i < 13){
+        columnThree.appendChild(imgDiv);
+    }else{
+        columnThree.appendChild(imgDiv);
+    }    
+}
+
+container.addEventListener('mousemove', (e) => {
+    let x = e.clientX - container.getBoundingClientRect().left;
+    let y = e.clientY - container.getBoundingClientRect().top;
+
+    canvas.style.transform = `translate(-${x}px, -${y}px)`
+})
+
+
